c6312deccb2db12f9fcd5a893ff4ee099bf5b4dc
Rana-Ramy <ramyrana504@gmail.com>
2026-06-17 05:18:53 +0300

    added a loop

--- main.js
+++ main.js
@@ -147,3 +147,25 @@
 
 /////////////////////////////////////////////////////////////////////////
 
+// function showInfo (user = "Un", age = "Un", rate = 0, show = "yes", ...skills){
+//     document.write(`<div>`);
+//     document.write('Welcome, ${user}')
+//     document.write('</div>');
+
+// }
+
+// showInfo(); ??????
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
