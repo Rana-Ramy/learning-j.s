770140b6dfcc3b85967a8003f84d65ec30362bf3
Rana-Ramy <ramyrana504@gmail.com>
2026-07-25 06:36:34 +0300

    Digital Clock program

--- main.js
+++ main.js
@@ -771,7 +771,9 @@ console.log(numbers);
 
 //////////////////////////////////////////////////////////////////////////////////
 
-let score = 0;
+function createGame() {
+
+    let score = 0;
 
 function increaseScore(points) {
     score += points;
@@ -783,5 +785,40 @@ function decreaseScore(points) {
     console.log(`-${points}pts`);
 }
 
-increaseScore(10);
-decreaseScore(5);
+function getScore() {
+    return score;
+}
+return {increaseScore, decreaseScore, getScore};
+}
+
+const game = createGame();
+
+game.increaseScore(10);
+game.decreaseScore(3);
+console.log(`The final score is: ${game.getScore()}pts`);
+
+/////////////////////////////////////////////////////////////////////////////////
+
+// Digital Clock program
+
+function updateClock(){
+    const now = new Date();
+    let hours = now.getHours();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds}:${meridiem}`
+    document.getElementById("clock").textContent = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
+
+////////////////////////////////////////////////////////////////////////////////
+
+//Stopwatch program
+
+
+
