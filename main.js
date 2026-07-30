1664ec50abe7889959fe5966e1b31d505558e0e6
Rana-Ramy <ramyrana504@gmail.com>
2026-07-30 07:08:55 +0300

    started a stopwatch program

--- main.js
+++ main.js
@@ -820,5 +820,31 @@ setInterval(updateClock, 1000);
 
 //Stopwatch program
 
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
 
+function start(){
 
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+
+    }
+
+}
+
+function stop(){
+
+}
+
+function reset(){
+
+}
+
+function update(){
+
+}
